06f89200b10872adbf26b2383d352d017d21f8d8
Ana Rita Norinho <up201606003@fe.up.pt>
2018-10-15 14:53:27 +0100

    Add files via upload

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -14,7 +14,7 @@
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
-#define MAX 3	
+#define MAX 3
 #define FLAG 0x7E
 int received=0;
 int llopen(int fd);
@@ -31,9 +31,9 @@ int main(int argc, char** argv)
 	set[0]= FLAG;
 	set[1]= 0x03; //A
 	set[2]= 0x03;//C, define a trama que se esta a usar, 0x03 - transmicao emissor -> recetor
-	set[3]= set[1]^set[2]; 
+	set[3]= set[1]^set[2];
 	set[4]=FLAG;
-	
+
     char buf[255];
     int i, sum = 0, speed = 0;
 
@@ -119,8 +119,13 @@ int main(int argc, char** argv)
       exit(-1);
     }
 */
-	 i =llopen(fd);
-	printf("llopen:: %d\n",i);
+char test[4];
+  test[0]=0x01;
+  test[1]=0x10;
+  test[2]=0x11;
+  test[3]=0x7E;
+	 llopen(fd);
+   //llwrite(fd,test,0x40);
 
     close(fd);
 
@@ -131,63 +136,118 @@ int llopen(int fd){
 	int res;
 	int i=0;
 	char buf[5];
-	
+
 	while(conta<MAX && !received){
 		desativa_alarme();
-		
+
 		res=write(fd,set,sizeof(set));
-		
+
 		alarm(3);
 		conta_zero();
 		while(!flag && !received){
-			
+
 			res=read(fd,buf+i,1);
-			
+
 		if (res!=0)
-			if(i<5 && res!=0) {	
+			if(i<5 && res!=0) {
 			switch(buf[i]){
 				case 0x7E:
 				if (i==0 || i==4)
-					i++;			
+					i++;
 				break;
 				default:
 				if (i!=0 && i!=4)
 					i++;
-				
+
 			}
 }
 			if (i==5){
 			if (buf[2]==0x07 && buf[3]==buf[1]^buf[2]) {
 				received=1;
 				desativa_alarme();
-				
+
 				}
 			}
 		}
 }
 }
 
-int llwrite(int buf){
- 
-	char set[255];
-	char or;
-	int i=0;
-while(!flag && !received){
-			
-			res=read(set,buf+i,1);
-		if (res!=0)
-			or=or^set[i];
-			i++;					
-			}
+int llwrite(int set,int fd,int trama){
 
-				
-				}
-			}
-		}
+	char buf1[255];
+  char buf2[255];
+  char buf3[255];
+	char bcc2;
+  int res;
+	int i,j;
+  i=0;
+  j=0;
+  while(res!=0){
+
+			res=read(set,buf1+i,1);
+      if (buf1[i]==0x7E){
+        buf2[j]=0x7D;
+        buf2[j+1]=0x5E;
+        j++;
+
+      }
+      else if(buf1[i]==0x7D){
+        buf2[j]=0x7D;
+        buf2[j+1]=0x5D;
+        j++;
+      }
+      else
+        {
+            buf2[j]=buf1[i];
+        }
+        j++;
+			bcc2=bcc2^buf1[i];
+			i++;
+	}
+  buf2[j]=bcc2;
+  buf3[0]=FLAG;
+  buf3[1]=0x03;
+  buf3[2]=trama;
+  buf3[3]=buf3[1]^buf3[2];
+  i=4;
+  while(i<j+4){
+    buf3[i]=buf2[j-4];
+    i++;
+  }
+  buf3[i]=FLAG;
+  i=0;
+  res=write(fd,buf3,sizeof(buf3));
+  char buf[255];
+  alarm(3);
+  conta_zero();
+  while(!flag && !received){
+
+    res=read(fd,buf+i,1);
+
+    if(res!=0) {
+    switch(buf[i]){
+      case 0x7E:
+      if (i==0 || i==4)
+        i++;
+      break;
+      default:
+      if (i!=0 && i!=4)
+        i++;
+
 }
-	
-	
+    if (i==5){
+    if (buf[2]==0x07 && buf[3]==buf[1]^buf[2]) {
+      received=1;
+      desativa_alarme();
+
+      }
+    }
+  }
+
 
 }
 
 
+
+
+}
