4a4d77608ff5c31cf0b23a86a697ba1ffb82920b
SmilingOwl <julianamcm05@gmail.com>
2018-11-05 18:57:30 +0000

    Add files via upload

--- llread.c
+++ llread.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <time.h>
 
+// MACROS
 #define BAUDRATE B38400
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
@@ -17,11 +18,25 @@
 #define REJ0 0x01
 #define REJ1 0x81
 
+// Variaveis globais
 int id_trama=0;
 void analyze_data(unsigned char * buffer, int size, int fd);
-
 volatile int STOP=FALSE;
 
+// Declarações de funções
+int main(int argc, char** argv);
+int llopen(int fd);
+int llread(int fd, unsigned char* buf2);
+int save_data(int fd);
+int analyze_start(unsigned char* buffer, int size);
+void analyze_data(unsigned char * buffer, int size, int fd);
+int llclose(int fd);
+
+/*
+* Base da camada de aplicação, é esta que controla todo o processo 
+* que ocorre nesta camada e que faz as chamadas às funções da camada 
+* de ligação.
+*/
 int main(int argc, char** argv)
 {
     int fd,c, res, i=0;
@@ -33,19 +48,12 @@ int main(int argc, char** argv)
   	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
       	printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
       	exit(1);
-    }
-
-
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
+    }  
   
-    
     fd = open(argv[1], O_RDWR | O_NOCTTY );
     if (fd <0) {perror(argv[1]); exit(-1); }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    if ( tcgetattr(fd,&oldtio) == -1) { 
       perror("tcgetattr");
       exit(-1);
     }
@@ -54,19 +62,10 @@ int main(int argc, char** argv)
     newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
     newtio.c_iflag = IGNPAR;
     newtio.c_oflag = 0;
-
-    /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
-
-
-
-  /* 
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) próximo(s) caracter(es)
-  */
+    newtio.c_cc[VTIME]    = 1;   
+    newtio.c_cc[VMIN]     = 0;  
 
     tcflush(fd, TCIOFLUSH);
 
@@ -75,29 +74,35 @@ int main(int argc, char** argv)
       	exit(-1);
     }
 
-
 	if(llopen(fd)==-1) { return -1; }
+
+	// para medição dos tempos 
 	struct timespec start_t, end_t;
-clock_gettime(CLOCK_REALTIME, &start_t);
+	clock_gettime(CLOCK_REALTIME, &start_t);
+	
 	save_data(fd);
 
 	clock_gettime(CLOCK_REALTIME, &end_t);
 	printf("time: %f\n", (double) (end_t.tv_sec-start_t.tv_sec + (end_t.tv_nsec-start_t.tv_nsec)/1E9));	
-	if(llclose(fd)==-1) { return -1; }
+	
+	if(llclose(fd)==-1)
+		return -1; 
 
-tcsetattr(fd,TCSANOW,&oldtio);
+	tcsetattr(fd,TCSANOW,&oldtio);
 
     close(fd);
     return 0;
 }
 
+/*
+* Lê trama de controlo SET e envia a trama UA.
+*/
 int llopen(int fd){
 
 	unsigned char SET[5], buf;
 	SET[0]  = 0x7E;
 	int i=0, res;
-	int state = 0; //fora
-
+	int state = 0; 
 
    	while(!STOP)
    	{
@@ -108,24 +113,37 @@ int llopen(int fd){
 			switch(state)
 			{
 			case 0:
-				if(buf == 0x7E) state++;
+				if(buf == 0x7E) 
+					state++;
 				break;
 			case 1:
-				if(buf != 0x7E) {state++; i++; SET[i] = buf;}
+				if(buf != 0x7E) {
+					state++; 
+					i++; 
+					SET[i] = buf;
+				}
 				break;
 			case 2:
-				if(buf != 0x7E && i >= 4) STOP = TRUE;
-				else if(buf != 0x7E){ i++; SET[i] = buf;}
-				else{ i++; SET[i] = buf; STOP = TRUE;}
+				if(buf != 0x7E && i >= 4) 
+					STOP = TRUE;
+				else if(buf != 0x7E){ 
+					i++; 
+					SET[i] = buf;
+				}
+				else{ 
+					i++;
+				 	SET[i] = buf; 
+				 	STOP = TRUE;
+				 }
 				break;
 			}
     	}
   	}
 
-
 	//analise
 	if(SET[3]!= SET[1]^SET[2] && SET[2]!=0x03)
 		return -1;
+
 	//estrutura a enviar UA
 	unsigned char UA[5];
 	UA[0] = 0x7E;
@@ -137,13 +155,16 @@ int llopen(int fd){
  	return 0;
 }
 
+/*
+* Lê tramas I e faz destuffing.
+*/
 int llread(int fd, unsigned char* buf2){
 	
     unsigned char buf;
     unsigned char trama[512];
     trama[0] = 0x7E;
     int i=0, res;
-    int state = 0; //fora
+    int state = 0; 
     STOP = FALSE;
 
    	while(!STOP)
@@ -155,21 +176,31 @@ int llread(int fd, unsigned char* buf2){
 			switch(state)
 			{
 			case 0:
-				if(buf == 0x7E) state++;
+				if(buf == 0x7E) 
+					state++;
 				break;
 			case 1:
-				if(buf != 0x7E) {state++; i++; trama[i] = buf;}
+				if(buf != 0x7E) {
+					state++; 
+					i++; 
+					trama[i] = buf;
+				}
 				break;
 			case 2:
-				if(buf != 0x7E){ i++; trama[i] = buf;}
-				else{ i++; trama[i] = buf; STOP = TRUE;}
+				if(buf != 0x7E){ 
+					i++; 
+					trama[i] = buf;
+				}
+				else{ 
+					i++; 
+					trama[i] = buf; 
+					STOP = TRUE;}
 				break;
 			}
     	}
   	}
-printf("N: %d\n", (int) trama[5]);
+	printf("N: %d\n", (int) trama[5]);
 	
-
 	unsigned char message[5];
 	message[0] = 0x7E;
 	message[1] = 0x03;
@@ -222,9 +253,11 @@ printf("N: %d\n", (int) trama[5]);
 		write(fd, message, 5);
 		return -1;
 	}
+
 	int n =0;
 	unsigned char bcc2;
 	int j = 4;
+
 	while(j < i-1)
 	{
     	if(trama[j] == 0x7D && trama[j+1] == 0x5E)	/*ciclo para apgar 0x5E e colocar 0x7D=0x7E */
@@ -257,6 +290,7 @@ printf("N: %d\n", (int) trama[5]);
 			write(fd, message, 5);
 			return -1;
 	}
+
 	else if(bcc2 != trama[i-1] && id_trama == 1)
 	{
 			printf("REJ1 entered\n");
@@ -265,7 +299,6 @@ printf("N: %d\n", (int) trama[5]);
 			write(fd, message, 5);
 			return -1;
 	}
-	
 
 	if(trama[2]==0x00 && id_trama == 0){
 		printf("RR1\n");
@@ -274,6 +307,7 @@ printf("N: %d\n", (int) trama[5]);
 		write(fd, message, 5);
 		return n-1;
 	}
+
 	if(trama[2]==0x40 && id_trama == 1){
 		printf("RR0\n");
 		message[2] = RR0;
@@ -284,7 +318,12 @@ printf("N: %d\n", (int) trama[5]);
 
 }
 
+/*
+* Função principal da transferência de dados que chama llread() e, 
+* de acordo com as tramas recebidas, chama as funções seguintes, que as analisarão.
+*/
 int save_data(int fd){
+
 	unsigned char buffer[512];
 	int stop2 = 0, size, fd1;
 	while(!stop2)
@@ -317,35 +356,41 @@ int save_data(int fd){
 	return 0;
 }
 
+/* Função que cria o ficheiro de acordo com a informação recebida.*/
 int analyze_start(unsigned char* buffer, int size) {
+	
 	int l1, l2, i=1;
+
 	if(buffer[1] == 0x00)
 	{
 		l1 =  (int) buffer[2];		
 	
 		if(buffer[3+l1]==0x01)
 		{
-			l2 = (int) buffer[4+l1]; //l2 = tamanho do nome do ficheiro
-
+			//l2 = tamanho do nome do ficheiro
+			l2 = (int) buffer[4+l1]; 
 		}
 	}
+
 	unsigned char nome[l2+1];
+
 	while(i <= l2)
 	{
 		nome[i-1]=buffer[4+l1+i];
 		i++;
 	}
-	nome[l2] = '\0';
 
+	nome[l2] = '\0';
 	int fd= open(nome, O_CREAT | O_WRONLY |O_APPEND);
 	return fd;
 }
 
+/* Função que copia a informação recebida para o ficheiro.*/
 void analyze_data(unsigned char * buffer, int size, int fd){
 
 	unsigned int l2=(unsigned int) buffer[2];
 	unsigned int l1=(unsigned int) buffer[3];
-
+	
 	int k = l2*256+l1; 
 	unsigned char toWrite[k];
 	int i=0;
@@ -358,7 +403,11 @@ void analyze_data(unsigned char * buffer, int size, int fd){
 	write(fd, toWrite, k);
 }
 
+/*
+* Lê trama de controlo DISC, envia DISC de volta e recebe UA.
+*/
 int llclose(int fd){
+
 	unsigned char DISC[5], buf;
 	DISC[0]  = 0x7E;
 	int i=0, res;
@@ -375,15 +424,27 @@ int llclose(int fd){
 			switch(state)
 			{
 			case 0:
-				if(buf == 0x7E) state++;
+				if(buf == 0x7E) 
+					state++;
 				break;
 			case 1:
-				if(buf != 0x7E) {state++; i++; DISC[i] = buf;}
+				if(buf != 0x7E) {
+					state++; 
+					i++; 
+					DISC[i] = buf;
+				}
 				break;
 			case 2:
-				if(buf != 0x7E && i >= 4) STOP = TRUE;
-				else if(buf != 0x7E){ i++; DISC[i] = buf;}
-				else{ i++; DISC[i] = buf; STOP = TRUE;}
+				if(buf != 0x7E && i >= 4) 
+					STOP = TRUE;
+				else if(buf != 0x7E){ 
+					i++; 
+					DISC[i] = buf;
+				}
+				else{ i++; 
+					DISC[i] = buf; 
+					STOP = TRUE;
+				}
 				break;
 			}
      	}
@@ -393,7 +454,6 @@ int llclose(int fd){
 	if(DISC[3]!= DISC[1]^DISC[2] && DISC[2]!=0x0B)
 		return -1;
    	res = write(fd, DISC, 5);
-
 
 	unsigned char UA[5];
 
@@ -409,12 +469,24 @@ int llclose(int fd){
 				if(buf == 0x7E) state++;
 				break;
 			case 1:
-				if(buf != 0x7E) {state++; i++; UA[i] = buf;}
+				if(buf != 0x7E) {
+					state++;
+				 	i++; 
+				 	UA[i] = buf;
+				}
 				break;
 			case 2:
-				if(buf != 0x7E && i >= 4) STOP = TRUE;
-				else if(buf != 0x7E){ i++; UA[i] = buf;}
-				else{ i++; UA[i] = buf; STOP = TRUE;}
+				if(buf != 0x7E && i >= 4) 
+					STOP = TRUE;
+				else if(buf != 0x7E){ 
+					i++; 
+					UA[i] = buf;
+				}
+				else{ 
+					i++; 
+					UA[i] = buf; 
+					STOP = TRUE;
+				}
 				break;
 			}
     	}
