9d98ac4f7856b96fe23ca39dbb827e64f2877888
Joana Silva <up201208979@fe.up.pt>
2018-11-05 12:11:55 +0000

    Add files via upload

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -27,20 +27,21 @@
 #define DISC 0x0B
 #define FILE_LENGTH 0x00
 #define FILE_NAME 0x01
-int received=0;
-int llopen(int fd);
-int create_start(int file_length,char *fileName,int fd);
-int create_data(int fd);
-int llwrite(int fd, char* set1,int size);
-int llclose(int fd);
-int create_end(int file_length,char *fileName,int fd);
 
-volatile int STOP=FALSE;
+
+int llopen(int fd);
+int create_start(int file_length,unsigned char *fileName,int fd);
+int create_data(int fd);
+int llwrite(int fd, unsigned char* set1,int size);
+int llclose(int fd);
+int create_end(int file_length,unsigned char *fileName,int fd);
 void atende();
-char set[5];
-char trama=0x00;
-char file[200];
-int n = 1;
+volatile int STOP=FALSE;
+int received=0;
+unsigned char set[5];
+unsigned char trama=0x00;
+unsigned char file[200];
+int n = 1; //sequenceNumber
 int k  = 260;
 
 int main(int argc, char** argv)
@@ -48,12 +49,12 @@ int main(int argc, char** argv)
 
     int fd,c, res;
     struct termios oldtio,newtio;
-	(void) signal(SIGALRM,atende);
-	set[0]= FLAG;
-	set[1]= 0x03; //A
-	set[2]= 0x03;//C, define a trama que se esta a usar, 0x03 - transmicao emissor -> recetor
-	set[3]= set[1]^set[2];
-	set[4]=FLAG;
+  	(void) signal(SIGALRM,atende);
+  	set[0]= FLAG;
+  	set[1]= 0x03; //A
+  	set[2]= 0x03;//C, define a trama que se esta a usar, 0x03 - transmicao emissor -> recetor
+  	set[3]= set[1]^set[2];
+  	set[4]=FLAG;
 
     char buf[255];
     int i, sum = 0, speed = 0;
@@ -70,7 +71,6 @@ int main(int argc, char** argv)
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
   */
-
 
     fd = open(argv[1], O_RDWR | O_NOCTTY );
     if (fd <0) {perror(argv[1]); exit(-1); }
@@ -98,8 +98,6 @@ int main(int argc, char** argv)
     leitura do(s) pr�ximo(s) caracter(es)
   */
 
-
-
     tcflush(fd, TCIOFLUSH);
 
     if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
@@ -107,9 +105,6 @@ int main(int argc, char** argv)
       exit(-1);
     }
 
-
-
-  llopen(fd);
 
 	/*printf("Please enter the name of the file: ");
 	scanf("%s",file);*/
@@ -121,17 +116,18 @@ int main(int argc, char** argv)
 	stat(file, &st);
 	file_size = st.st_size;
 
-    create_start(file_size, file,fd);
+  llopen(fd);
+  create_start(file_size, file,fd);
 	create_data(fd);
-    create_end(file_size,file,fd);
-    llclose(fd);
-    close(fd);
+  create_end(file_size,file,fd);
+  llclose(fd);
+  close(fd);
 
   return 0;
 }
-int create_start(int file_length,char *fileName,int fd){
+int create_start(int file_length,unsigned char *fileName,int fd){
 
-  char pack_start[256];
+  unsigned char pack_start[256];
   pack_start[0]=START;
   int i = 0;
   unsigned char v1[256];
@@ -150,7 +146,7 @@ int create_start(int file_length,char *fileName,int fd){
   i+=3;
   if (fileName != NULL){
     pack_start[i]=FILE_NAME; //t2
-	 	 int str_length=strlen(fileName);
+	 	int str_length=strlen(fileName);
 
     i++;
     pack_start[i]=str_length; //l2
@@ -158,25 +154,21 @@ int create_start(int file_length,char *fileName,int fd){
 
     for (j = 0;j<str_length;j++){
       pack_start[i]=fileName[j]; //v2
-
-
       i++;
     }
   }
 
     int res = llwrite(fd,pack_start,i+1);
     trama=0x40;
-
-
 }
 
 
 int create_data(int fd){
-  char pack_data[260];
+  unsigned char pack_data[k];
   int i =0;
-  char dest[260];
-  char filename[20];
-  
+  unsigned char dest[k];
+  unsigned char filename[20];
+
   strcpy(filename,file);
   int open_file = open(filename,O_RDONLY);
   int res;
@@ -186,26 +178,23 @@ int create_data(int fd){
 
   pack_data[i]=DATA;
   i++;
-  pack_data[i] = (int) n;
+  pack_data[i] = (unsigned int) n;
   i++;
-  pack_data[i] = (int) res / 256; //l2
+  pack_data[i] = (unsigned int) res / 256; //l2
   i++;
-  pack_data[i] = (int) res % 256; //l1
+  pack_data[i] = (unsigned int) res % 256; //l1
   i++;
 
 	int j=0;
-
 	for(j;j<res;j++){
 	  pack_data[i] = dest[j];
-  //  printf("packdata: %x\n", pack_data[i]);
 		i++;
 	}
-
 
   int res = llwrite(fd,pack_data,i+1);
 
   n++;
-  printf("N: %d\n",n );
+  printf("Sequence Number N: %d\n",n );
   i=0;
   if ((n%2)==0)
     trama=0x00;
@@ -215,17 +204,15 @@ int create_data(int fd){
   }
 
 }
-int create_end(int file_length,char *fileName,int fd){
+int create_end(int file_length,unsigned char *fileName,int fd){
   unsigned char pack_end[256];
   pack_end[0]=END;
   int i = 0;
   unsigned char v1[256];
-  printf("entered create end!\n");
   do {
         v1[0]=(int)(file_length/pow(256.0,(double)i)) % 256;
         i++;
   }while(v1[i]!=0);
-  printf("exited while in create end!\n");
 
   pack_end[1]= FILE_LENGTH;
   pack_end[2]=i;
@@ -234,7 +221,7 @@ int create_end(int file_length,char *fileName,int fd){
   for (j=3;j<i;j++){
     pack_end[j]=v1[j-3];
   }
-  printf("exited for in create end!\n");
+
   i+=4;
   if (fileName != NULL){
     pack_end[i]=FILE_NAME;
@@ -242,48 +229,43 @@ int create_end(int file_length,char *fileName,int fd){
     i++;
     pack_end[i]=str_length;
     i++;
-    for (j = 0;j<str_length;j++){
+    for(j = 0;j<str_length;j++){
       pack_end[i]=fileName[j];
       i++;
     }
-	printf("exited last for in create end!\n");
   }
 
-    int res = llwrite(fd,pack_end,i+1);
-	printf("end of create end!\n");
-
+  int res = llwrite(fd,pack_end,i+1);
 }
 
 int llopen(int fd){
 	int res;
 	int i=0;
-	char buf[5];
+	unsigned char buf[5];
 	conta_zero();
+
 	while(conta<MAX && !received){
 		desativa_alarme();
 
 		res=write(fd,set,sizeof(set));
-		//printf("res: %d \n", res);
-
 
 		alarm(3);
 		while(!flag && !received){
 
 			res=read(fd,buf+i,1);
-			//printf("res: %d \n", res);
 
-		if (res!=0)
-			if(i<5 && res!=0) {
-			switch(buf[i]){
-				case 0x7E:
-				if (i==0 || i==4)
-					i++;
-				break;
-				default:
-				if (i!=0 && i!=4)
-					i++;
+  		if (res!=0)
+  			if(i<5 && res!=0) {
+  			switch(buf[i]){
+  				case 0x7E:
+  				if (i==0 || i==4)
+  					i++;
+  				break;
+  				default:
+  				if (i!=0 && i!=4)
+  					i++;
 
-			}
+  			}
 		}
 			if (i==5){
 			if (buf[2]==0x07 && buf[3]==buf[1]^buf[2]) {
@@ -297,7 +279,7 @@ int llopen(int fd){
 }
 
 
-int llwrite(int fd, char* set1,int size){
+int llwrite(int fd,unsigned char* set1,int size){
 
   unsigned char buf1[512];
   unsigned char buf2[512];
@@ -308,8 +290,7 @@ int llwrite(int fd, char* set1,int size){
 	int i,j;
   i=0;
   j=0;
-flag=1;
-	printf("entered llwrite\n");
+  flag=1;
  	for(; i<size;i++){
 		buf1[i]=set1[i];
 
@@ -337,7 +318,7 @@ flag=1;
 
 
 	}
-printf("exited for in llwrite\n");
+
   int aux2=i;
   buf2[j]=bcc2;
   buf3[0]=FLAG;
@@ -345,42 +326,33 @@ printf("exited for in llwrite\n");
   buf3[2]=trama;
   buf3[3]=buf3[1]^buf3[2];
   i=4;
-if (trama ==0x00){
-		confirmation=RR1;
 
-}
+if (trama ==0x00)
+		confirmation=RR1;
 else
 	confirmation=RR0;
 
-printf("before while in llwrite\n");
-
   while(i<=j+4){
-
     buf3[i]=buf2[i-4];
     i++;
   }
 
-printf("after while  in llwrite\n");
   buf3[i]=FLAG;
-
   conta_zero();
   received=0;
   int aux = i+1;
-printf("before while in llwrite\n");
+
   while(conta <= MAX && !received){
-
   desativa_alarme();
-  
-  res=write(fd,buf3,aux);
-  printf("numero %d\n", res);
 
+  res=write(fd,buf3,aux);
   i=0;
+
   unsigned char buf[512];
   alarm(3);
   flag=0;
   while(!flag && !received){
   res=read(fd,buf+i,1);
-
 
     if(res!=0) {
     switch(buf[i]){
@@ -391,60 +363,45 @@ printf("before while in llwrite\n");
       default:
       if (i!=0 && i!=4)
         i++;
-
 }
-
     if (i==5){
-      
-
-    if (buf[2] == confirmation && buf[3]==buf[1]^buf[2]) {
-      received=1;
-      //stop=1;
-      //printf("Received confirmation - deactivating the alarm...\n");
-      desativa_alarme();
-
-      }
-
-
+      if (buf[2] == confirmation && buf[3]==buf[1]^buf[2]) {
+        received=1;
+        desativa_alarme();
+        }
       else if(buf[2]== REJ0 && trama == 0x00){
        printf("Restransmiting - received REJ0\n");
        return llwrite(fd,set1,size);
-      }else if(buf[2]== REJ1 && trama == 0x40){
+      }
+      else if(buf[2]== REJ1 && trama == 0x40){
        printf("Restransmiting - received REJ1\n");
       return llwrite(fd,set1,size);
       }
   }
 }
-
 }
-printf("after while  in llwrite\n");
-
 }
-printf("received final %d\n", received);
+
 if(received == 0){
   printf("timeout!\n");
   exit(0);
 }
-
 return 0;
 }
 
 int llclose(int fd){
 	int res;
 	int i=0;
-	char buf[5];
+	unsigned char buf[5];
 	conta_zero();
-  	set[2]=DISC;
+  set[2]=DISC;
 	set[3] = set[1] ^ set[2];
-  	received = 0;
+  received = 0;
 
-	printf("entered llclose!\n");
 	while(conta<MAX && !received){
 		desativa_alarme();
-	
+
 		res=write(fd,set,sizeof(set));
-
-
 
 		alarm(3);
 		while(!flag && !received){
@@ -468,14 +425,12 @@ int llclose(int fd){
 			if (buf[2]==DISC && buf[3]==buf[1]^buf[2]) {
 				received=1;
         set[2]=0x07;
-		set[3] = set[1] ^ set[2];
+		    set[3] = set[1] ^ set[2];
         res=write(fd,set,sizeof(set));
 				desativa_alarme();
-
 				}
 			}
 		}
-
 
 printf("Llclose successful!\n");
 }
