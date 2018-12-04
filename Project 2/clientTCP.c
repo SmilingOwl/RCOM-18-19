e5e87a9389c357d17cd35d7383187c89f6ee9d36
Joana Silva <up201208979@fe.up.pt>
2018-12-04 12:27:34 +0000

    added getCommand

--- Project 2/clientTCP.c
+++ Project 2/clientTCP.c
@@ -15,9 +15,11 @@
 #define SERVER_ADDR "192.168.28.96"
 #define MAX_SIZE 256
 
+
 struct hostent *getip( char host[]);
 int parseURL(char url[], char *user, char *password, char *host, char *url_path);
 int sendCommand(int fd_socket, char *command);
+int getCommand(char *command, int index, char *user, char *password, char *host, char *url_path);
 
 int main(int argc, char** argv){
 
@@ -27,47 +29,56 @@ char user[MAX_SIZE];
 char password[MAX_SIZE];
 char host[MAX_SIZE];
 char url_path[MAX_SIZE];
+char command[MAX_SIZE];
 
 parseURL(argv[1], user, password, host, url_path);
+
+getCommand(command, 0, user, password, host, url_path);
+getCommand(command, 1, user, password, host, url_path);
+getCommand(command, 2, user, password, host, url_path);
+getCommand(command, 3, user, password, host, url_path);
+getCommand(command, 4, user, password, host, url_path);
+getCommand(command, 5, user, password, host, url_path);
+
 
 printf("User: %s\n",user);
 printf("Password: %s\n",password);
 printf("Host: %s\n",host);
 printf("Url path: %s\n",url_path);
-
-  	int	sockfd;
-  	struct	sockaddr_in server_addr;
-  	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
-  	int	bytes;
-
-  	/*server address handling*/
-  	bzero((char*)&server_addr,sizeof(server_addr));
-  	server_addr.sin_family = AF_INET;
-  	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
-  	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
-
-  	/*open an TCP socket*/
-  	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
-      		perror("socket()");
-          	exit(0);
-      	}
-  	/*connect to the server*/
-      	if(connect(sockfd,
-  	           (struct sockaddr *)&server_addr,
-  		   sizeof(server_addr)) < 0){
-          	perror("connect()");
-  		exit(0);
-  	}
-
-  	/*send a string to the server*/
-  	bytes = write(sockfd, buf, strlen(buf));
-  	printf("Bytes escritos %d\n", bytes);
-
-
-		//h = getip(host);
-
-
-  	close(sockfd);
+//
+//   	int	sockfd;
+//   	struct	sockaddr_in server_addr;
+//   	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
+//   	int	bytes;
+//
+//   	/*server address handling*/
+//   	bzero((char*)&server_addr,sizeof(server_addr));
+//   	server_addr.sin_family = AF_INET;
+//   	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
+//   	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
+//
+//   	/*open an TCP socket*/
+//   	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+//       		perror("socket()");
+//           	exit(0);
+//       	}
+//   	/*connect to the server*/
+//       	if(connect(sockfd,
+//   	           (struct sockaddr *)&server_addr,
+//   		   sizeof(server_addr)) < 0){
+//           	perror("connect()");
+//   		exit(0);
+//   	}
+//
+//   	/*send a string to the server*/
+//   	bytes = write(sockfd, buf, strlen(buf));
+//   	printf("Bytes escritos %d\n", bytes);
+//
+//
+// 		//h = getip(host);
+//
+//
+//   	close(sockfd);
   	exit(0);
  }
 
@@ -134,9 +145,48 @@ int parseURL(char url[], char *user, char *password, char *host, char *url_path)
 	return 0;
 }
 
+int getCommand(char *command, int index, char *user, char *password, char *host, char *url_path){
+
+  switch (index) {
+    case 0:
+      strcpy(command, "telnet ");
+      strcat(command, url_path);
+      strcat(command, " 110");
+      break;
+    case 1:
+      strcpy(command, "user ");
+      strcat(command, user);
+      break;
+    case 2:
+      strcpy(command, "pass ");
+      strcat(command, password);
+      break;
+    case 3:
+      strcpy(command, "stat");
+      break;
+    case 4:
+      strcpy(command, "retr ");
+      //strcat(command, )
+      break;
+    case 5:
+      strcpy(command, "quit");
+      break;
+    default:
+    break;
+
+  }
+
+  printf("command: %s\n", command);
+
+
+return 0;
+
+}
+
 int sendCommand(int fd_socket, char *command){
 
 	write(fd_socket, command, sizeof(command));
+
 
 
 }
