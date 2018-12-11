5c3a0fa9b3e9f489bb94bd7916d7ca6b43886e2e
helenaMontenegro <36671963+helenaMontenegro@users.noreply.github.com>
2018-12-11 12:35:31 +0000

    Add files via upload

--- Project 2/clientTCP.c
+++ Project 2/clientTCP.c
@@ -1,5 +1,4 @@
-/*      (C)2000 FEUP  */
-
+#include <string.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -9,13 +8,13 @@
 #include <unistd.h>
 #include <signal.h>
 #include <netdb.h>
-#include <string.h>
+#include <strings.h>
 #include <ctype.h>
+#include <fcntl.h>
 
-#define SERVER_PORT 6000
+#define SERVER_PORT 21
 #define SERVER_ADDR "192.168.28.96"
 #define MAX_SIZE 256
-
 
 struct hostent *getip( char host[]);
 int parseURL(char url[], char *user, char *password, char *host, char *url_path);
@@ -23,55 +22,59 @@ int sendCommand(int fd_socket, char *command);
 int getCommand(char *command, int index, char *user, char *password, char *host, char *url_path);
 int connection(int fd_socket, char *user, char *password, char *host, char *url_path);
 int readAnswer(int fd_socket, unsigned char *answer);
+void createNewFile(int fd_socket, char *file_name);
+void parseFilename(char *path, char *filename);
+
+char *file_name;
 
 int main(int argc, char** argv){
 
-struct hostent *h;
+	struct hostent *h;
+	int sockfd;
+  	struct sockaddr_in server_addr;
 
-char user[MAX_SIZE];
-char password[MAX_SIZE];
-char host[MAX_SIZE];
-char url_path[MAX_SIZE];
-char command[MAX_SIZE];
+	char user[MAX_SIZE];
+	char password[MAX_SIZE];
+	char host[MAX_SIZE];
+	char url_path[MAX_SIZE];
+	char command[MAX_SIZE];
+	char filename[MAX_SIZE];
 
-parseURL(argv[1], user, password, host, url_path);
+	parseURL(argv[1], user, password, host, url_path);
+	parseFilename(argv[1],filename);
+	file_name = filename;
 
-h = getip(host);
+	h = getip(host);
 
+	printf("User: %s\n",user);
+	printf("Password: %s\n",password);
+	printf("Host: %s\n",host);
+	printf("Url path: %s\n",url_path);
+	printf("Filename:%s\n",file_name);
+	
 
-printf("User: %s\n",user);
-printf("Password: %s\n",password);
-printf("Host: %s\n",host);
-printf("Url path: %s\n",url_path);
+/*server address handling*/
+	bzero((char *)&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)h->h_addr))); /*32 bit Internet address network byte ordered*/
+	server_addr.sin_port = htons(SERVER_PORT);											/*server TCP port must be network byte ordered */
 
-  	int	sockfd;
-  	struct	sockaddr_in server_addr;
-  	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
-  	int	bytes;
-
-
-    /*server address handling*/
-  	bzero((char*)&server_addr,sizeof(server_addr));
-  	server_addr.sin_family = AF_INET;
-  	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
-  	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
-
-  	/*open an TCP socket*/
-  	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
-      		perror("socket()");
-          	exit(0);
-      	}
-  	/*connect to the server*/
-      	if(connect(sockfd,
-  	           (struct sockaddr *)&server_addr,
-  		   sizeof(server_addr)) < 0){
-          	perror("connect()");
-  		exit(0);
-  	}
+	/*open an TCP socket*/
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("socket()");
+		exit(0);
+	}
+	/*connect to the server*/
+	if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	{
+		perror("connect()");
+		exit(0);
+	}
 
   	/*send a string to the server*/
-  //	bytes = write(sockfd, buf, strlen(buf));
-  //	printf("Bytes escritos %d\n", bytes);
+    sendCommand(sockfd, command);
+ 
 
 
     connection(sockfd, user, password, host, url_path);
@@ -84,21 +87,17 @@ printf("Url path: %s\n",url_path);
  }
 
 
-   struct hostent *getip( char host[]){
-          struct hostent *h;
+struct hostent *getip( char host[]){
+    struct hostent *h;
 
-         if ((h=gethostbyname(host)) == NULL) {
-             herror("gethostbyname");
-             exit(1);
-        }
+    if ((h=gethostbyname(host)) == NULL) {
+        herror("gethostbyname");
+        exit(1);}
 
-        printf("Host name  : %s\n", h->h_name);
-        printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
+    return h;
+}
 
-       return h;
- }
-
-	  //ftp:<user>:<password>@<host>/<url-path>.
+//ftp:<user>:<password>@<host>/<url-path>.
 int parseURL(char url[], char *user, char *password, char *host, char *url_path){
 
 	int i=6, j=0;
@@ -131,10 +130,10 @@ int parseURL(char url[], char *user, char *password, char *host, char *url_path)
 		i++;
 		j++;
 	}
-
 	host[j] = '\0';
 	i++;
 	j=0;
+	
 	while(url[i]!='\0'){
 		url_path[j]=url[i];
 		i++;
@@ -146,62 +145,74 @@ int parseURL(char url[], char *user, char *password, char *host, char *url_path)
 	return 0;
 }
 
+void parseFilename(char *path, char *filename){
+	int i=0;
+	int j=0;
+	int boolean=0;
+
+while(path[i]!='\0'){
+	if(path[i]=='-'){
+		boolean=1;
+		i++;
+		continue;
+	}
+	if(boolean){
+		filename[j]=path[i];
+		j++;
+	}
+	i++;
+}
+}
+
 int getCommand(char *command, int index, char *user, char *password, char *host, char *url_path){
 
   switch (index) {
     case 0:
-      strcpy(command, "telnet ");
-      strcat(command, url_path);
-      strcat(command, " 110");
-      break;
-    case 1:
       strcpy(command, "user ");
       strcat(command, user);
       break;
-    case 2:
+    case 1:
       strcpy(command, "pass ");
       strcat(command, password);
       break;
+    case 2:
+      strcpy(command, "pasv");
+      break;
     case 3:
-      strcpy(command, "stat");
-      break;
-    case 4:
-      strcpy(command, "retr ");
-      //strcat(command, )
-      break;
-    case 5:
-      strcpy(command, "quit");
+      strcpy(command, "retr");
       break;
     default:
-    break;
-
+      break;
   }
 
   printf("command: %s\n", command);
-
-
-return 0;
-
-}
+return 0;}
 
 int sendCommand(int fd_socket, char *command){
   char answer[512];
 
-	write(fd_socket, command, sizeof(command));
+  write(fd_socket, command, sizeof(command));
   readAnswer(fd_socket, answer);
 
-  if(answer == '1'){
-    if(strcmp(command,"retr ") == 0){
-    //  createNewFile(fd_socket, file_name);
-    }else  readAnswer(fd_socket, answer);
+printf("%s\n", answer);
 
-    }else if(answer == '2'){
+  if(answer[0] == '1'){
+	printf("entered 1");
+   
+    }else if(answer[0] == '2'){
+	if(strcmp(command,"pasv") == 0){
+	readSocketfd(fd_socket);
+      		createNewFile(fd_socket, file_name);
+    	}//}else  //readAnswer(fd_socket, answer);
+printf("entered 2");
       return 0;
-    }else if(answer == '3'){
+    }else if(answer[0] == '3'){
+printf("entered 3");
       return 1;
-    }else if(answer == '4'){
+    }else if(answer[0] == '4'){
+printf("entered 4");
       sendCommand(fd_socket, command);
-    }else if(answer == '5'){
+    }else if(answer[0] == '5'){
         close(fd_socket);
         exit(-1);
     }
@@ -210,9 +221,9 @@ int sendCommand(int fd_socket, char *command){
 }
 
 int connection(int fd_socket, char *user, char *password, char *host, char *url_path){
-  unsigned int i = 1;
+  unsigned int i = 0;
 
-  while(i <= 5){
+  while(i <= 3){
 
 
     char command[512];
@@ -225,7 +236,7 @@ int connection(int fd_socket, char *user, char *password, char *host, char *url_
   return 0;
 }
 
-  int readAnswer(int fd_socket, unsigned char *answer){
+int readAnswer(int fd_socket, unsigned char *answer){
 
     unsigned char response[3];
     unsigned char aux;
@@ -239,15 +250,15 @@ int connection(int fd_socket, char *user, char *password, char *host, char *url_
         response[i] = aux;
         i++;
 
-      }else if(aux == "\n"){
+      }else if(aux == '\n'){
 
           break;
 
-      }else if(i == 3 && aux =="-"){
+      }else if(i == 3 && aux =='-'){
           i = 0;
       }
 
-      printf("%c\n", aux);
+      //printf("%c\n", aux);
     }
 
 
@@ -255,13 +266,56 @@ int connection(int fd_socket, char *user, char *password, char *host, char *url_
     return 0;
   }
 
-  void createNewFile(int fd, char* name){
+void createNewFile(int fd, char* name){
 
-    fd = open(name,O_CREAT);
+    fd = open(name,O_CREAT|O_APPEND|O_WRONLY);
     if(fd == -1){
       printf("Error creating a new file...\n");
       exit(-1);
     }
+printf("open\n");
+}
+
+int readSocketfd(int sockfd) {
+	unsigned char response[3];
+    unsigned char aux2[4], aux;
+    unsigned int i = 0, state = 0, first_byte, second_byte;
 
 
-  }
+    while(read(sockfd, &aux, 1) != 0){
+
+      if(isdigit(aux)){
+	if(state > 0) {
+		aux2[i] = aux;
+	}
+
+        response[i] = aux;
+        i++;
+
+      }else if(aux == '\n'){
+          break;
+
+      }else if(i == 3 && aux ==' '){
+          i = 0;
+      }else if(aux ==','){
+          i = 0;
+	first_byte = atoi(aux2);
+	int j=0;	
+	while(j < 4){
+		aux2[j] = '\0';
+		j++;
+	}
+      }else if(aux ==')'){
+	second_byte = atoi(aux2);
+      }
+	
+    }
+printf("first_byte: %d\n", first_byte);
+	printf("second_byte: %d\n", second_byte);
+	return first_byte*256 + second_byte;
+
+}
+
+void savingFile(int fd){
+	
+}
